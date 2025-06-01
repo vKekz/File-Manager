c9925bcdec25ead2b9db05bcb9bd365848cfbbef
vKekz <kekzcrafter@protonmail.com>
2025-06-01 03:47:11 +0200

    Style adjustments to auth form

--- Frontend/src/app/components/user-auth-form/user-auth-form.component.ts
+++ Frontend/src/app/components/user-auth-form/user-auth-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, ElementRef, Input, ViewChild } from "@angular/core";
 import { AuthFormEnum } from "../../../enums/auth-form.enum";
 import { ButtonComponent } from "../button/button.component";
 import { IconComponent } from "../icon/icon.component";
@@ -14,13 +14,33 @@ export class UserAuthFormComponent {
   @Input()
   public type: AuthFormEnum = AuthFormEnum.LOGIN;
 
+  @ViewChild("userNameInput")
+  private userNameInput?: ElementRef;
+
+  @ViewChild("emailInput")
+  private emailInput?: ElementRef;
+
+  @ViewChild("passwordInput")
+  private passwordInput?: ElementRef;
+
   protected readonly iconSize: number = 64;
   protected readonly AuthFormEnum = AuthFormEnum;
 
   constructor(private readonly router: Router) {}
 
+  protected handleSubmit(event: Event) {
+    const submitEvent = event as SubmitEvent;
+    submitEvent.preventDefault();
+
+    const userName = (this.userNameInput?.nativeElement as HTMLInputElement).value;
+    const email = (this.emailInput?.nativeElement as HTMLInputElement).value;
+    const password = (this.passwordInput?.nativeElement as HTMLInputElement).value;
+  }
+
   protected async goToOtherAuthForm() {
     const route = this.type == AuthFormEnum.LOGIN ? "/signup" : "/login";
     await this.router.navigateByUrl(route);
   }
+
+  private handleAuth() {}
 }
